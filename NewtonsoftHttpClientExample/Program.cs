fc88c5e4ace4d712d024bce4008709f7398aefc6
Maclain Wiltzer <mwiltzer@gmail.com>
2021-07-21 11:07:14 -0400

    Added example console app

--- NewtonsoftHttpClientExample/Program.cs
+++ NewtonsoftHttpClientExample/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.HttpClientExtensions;
+using Newtonsoft.Json.Converters;
+using System.Threading.Tasks;
 
 namespace NewtonsoftHttpClientExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+
+            var stock = await httpClient.GetFromJsonAsync<Stock>("https://localhost:12345/stocks/VTSAX");
+            Console.WriteLine($"Got stock {stock.Symbol}. Current price = {stock.Price}.");
+            stock.Price += 0.10m;
+
+            await httpClient.PostAsJsonAsync<Stock>("https://localhost:12345/stocks/", stock);
+            Console.WriteLine("Added 10 cents to the price");
         }
     }
 }
---
+++ NewtonsoftHttpClientExample/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewtonsoftHttpClientExample
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset QuoteTime { get; set; }
+        public FundTypes FundType { get; set; }
+    }
+    public enum FundTypes
+    {
+        Stock,
+        MutualFund,
+        ETF
+    }
+}
