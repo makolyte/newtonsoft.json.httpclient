63b953f95a45ecf029674b8b9a3e59ee037f4bae
Maclain Wiltzer <mwiltzer@gmail.com>
2021-07-21 08:07:44 -0400

    Add two extension methods that this'll support

--- Newtonsoft.Json.HttpClient/NewtonsoftHttpClientExtensions.cs
+++ Newtonsoft.Json.HttpClient/NewtonsoftHttpClientExtensions.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading;
 
-namespace Newtonsoft.Json.HttpClient
+namespace Newtonsoft.Json.HttpClientExtensions
 {
     public static class NewtonsoftHttpClientExtensions
     {
+        public static Task<T> GetFromJsonAsync<T>(this HttpClient httpClient, string uri, JsonSerializerSettings settings = null, CancellationToken cancellationToken = default)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient httpClient, string uri, T value, JsonSerializerSettings settings = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
