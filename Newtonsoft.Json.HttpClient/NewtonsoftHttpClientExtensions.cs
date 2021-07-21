78396ce78c5b822b1f7da53eeaed7642adf69cbf
Maclain Wiltzer <mwiltzer@gmail.com>
2021-07-21 07:51:19 -0400

    Init all the projects

---
+++ Newtonsoft.Json.HttpClient/NewtonsoftHttpClientExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Newtonsoft.Json.HttpClient
+{
+    public static class NewtonsoftHttpClientExtensions
+    {
+    }
+}
---
+++ Newtonsoft.Json.HttpClientTests/NewtonsoftHttpClientExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.HttpClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newtonsoft.Json.HttpClient.Tests
+{
+    [TestClass()]
+    public class NewtonsoftHttpClientExtensionsTests
+    {
+        [TestMethod()]
+        public void Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
